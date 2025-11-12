c974b8711771c445a2ed220eb6e8e6157e942d16
Patricia <patr1541@stud.ek.dk>
2025-11-12 14:48:36 +0100

    forside, cards, img commit

--- js/forms.js
+++ js/forms.js
@@ -1,1 +1,24 @@
 console.log("Loaded forms javascrips site...");
+
+const myForm = document.querySelector("webform");
+
+function cancelPopup(event) {
+  event.preventDefault();
+  myForm.querySelector(":user-invalid").focus();
+}
+
+myForm.addEventListener("invalid", cancelPopup, true);
+
+/*Indsæt info fra <form id="webform">
+          <div class="form-group_kontakt"> til id="form-summary */
+
+function handleSubmit(event) {
+  // 1. undgå refresh
+  event.preventDefault();
+  // 2. samle data op
+  // 3. vise data
+  // 4. evt. nustil formular
+  myForm.reset();
+}
+
+myForm.addEventListener("submit", handleSubmit);
--- js/instruction.js
+++ js/instruction.js
@@ -5,7 +5,8 @@ document.querySelector("#oejeknap").addEventListener("click", oejefunktion);
 function oejefunktion() {
   console.log("Loaded instructions ØJE...");
   document.querySelector("#hotspotEye").classList.add("my_scale");
-  document.querySelector("#efficiency").textContent = "peter siger fucking øje";
+  document.querySelector("#efficiency").textContent =
+    "Øjet: Den har kun et øje og kan kun se 15 meter ud i luften, dog kan den se meget skarpt og igennem ting, så du kan ikke gemme dig, når den er i nærheden.";
 }
 
 document.querySelector("#fodknap").addEventListener("click", fodfunktion);
@@ -14,7 +15,7 @@ function fodfunktion() {
   console.log("Loaded instructions FØDDER...");
   document.querySelector("#hotspotFoot").classList.add("my_scale");
   document.querySelector("#efficiency").textContent =
-    "peter siger fucking fødder";
+    "Fødder: Vandmandens ben er meget lange og tynde, og den kan derfor løbe ligeså hurtigt som en gepard og hurtigt fange dig, hvis du forsøger at flygte.  Dog, kan den kun danse 15 minutter ad gangen, så hvis du ser den, er du 'kun' tvunget til 15 minutters dans ad gangen, også er du fredet en hel time.";
 }
 
 document
@@ -25,5 +26,5 @@ function tentfunktion() {
   console.log("Loaded instructions TENTAKLER...");
   document.querySelector("#hotspotTent").classList.add("my_scale");
   document.querySelector("#efficiency").textContent =
-    "peter siger fucking tentakler";
+    "Tentakler: Hendes tentakler kan svitse, så hvis man prøver at angribe vandmanden, bliver man svitset af dens tentaklerne. Dog er den kilden, så hvis man kommer tæt nok på under flamenco-dansen, kan man måske gå den til at grine og hermed blive fredet";
 }
